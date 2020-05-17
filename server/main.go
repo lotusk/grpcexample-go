289e0fba88d159a2e6fb64199afe0e1cf64e4e2f
Kai Wang <kai.wang@prometheanworld.com>
2020-05-17 13:59:50 +0800

    add grpc example

---
+++ server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"runtime/debug"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+
+	pb "../helloworld"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	port = ":50051"
+)
+
+func RecoveryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+			err = status.Errorf(codes.Internal, "Panic err: %v", e)
+		}
+	}()
+
+	return handler(ctx, req)
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	var testcrash map[string]string
+	if in.GetName() == "crash" {
+		testcrash["abc"] = "123"
+	}
+
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	opts := []grpc.ServerOption{
+		grpc_middleware.WithUnaryServerChain(RecoveryInterceptor),
+	}
+	s := grpc.NewServer(opts...)
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
